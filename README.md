dcba5a9fdac0ffba2e72458cad8dbb56b8fff078
Akihiko Ito <a2-ito@nri.co.jp>
2019-11-10 11:21:44 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# image-splitter
+
+## Overview
+
+This shell script will be splitting scaned images like books by using Imagemagick automatically. It worked with bash on WSL Ubuntu 16.04 environment.
+All images that will not be required to split will be adding the list called ```IGNORE_LIST``` for retrying.
+
+## Parameters
+|Name|Description|
+|:---|:----|:----|
+|```IGNORE_LIST```|the file will be added by the script automatically|
+|```SPLIT_RATE```|the percent to split the width of the image|
+|```_image_parentparent_dir```|the directory path stroing images|
+
---
+++ image-splitter.sh
@@ -0,0 +1,91 @@
+IFS=$'\n'
+
+############################################################################################
+IGNORE_LIST=ignore.lst
+SPLIT_RATE=50
+_image_parentparent_dir=""
+############################################################################################
+
+image_splitter() {
+  _image_dir=$1
+  _split_rate_left=`echo "scale=5; 100-${SPLIT_RATE}"|bc`
+
+  _dirname=`basename "${_image_dir}"`
+  if [ -e "${_dirname}" ]; then
+    echo "  "skip - "${_dirname}"
+		return
+  fi
+  _number_files=`ls -l "${_image_dir}" | wc -l`
+  mkdir -p "${_dirname}"
+  _count=0
+  for _image in `find "${_image_dir}" -maxdepth 1 -type f -not -name "*.db"`;
+  do
+    _count=`expr ${_count} + 1`
+    _height=`identify -format '%h' "${_image}"`
+		if [ -z ${_height} ]; then
+			echo "${dirname}" "${_image_filename}"
+			break
+		fi
+    _width=`identify -format '%w' "${_image}"`
+    _image_filename=`basename "${_image}"`
+    _fext="${_image_filename##*.}"
+    _fname="${_image_filename%.*}"
+    _progress=`echo "scale=5; ${_count}/${_number_files}*100" | bc`
+    printf "                                                                      \r"
+    printf "  %-20s progress:[%5.1f%% %4d/%4d] %15s\r" \
+      "${_dirname}" ${_progress} ${_count} ${_number_files} "${_fname}"
+    if [ ${_width} -gt ${_height} ]; then
+      _split_width=`echo "scale=5; ${_width}*${SPLIT_RATE}/100"|bc`
+      _output_image_name_right=${_fname}-1.${_fext}
+      _output_image_name_left=${_fname}-2.${_fext}
+      convert -crop ${SPLIT_RATE}%x100%+0+0 "${_image}" "${_dirname}/${_output_image_name_left}"
+      convert -crop ${_split_rate_left}%x100%+${_split_width}%+0 "${_image}" "${_dirname}/${_output_image_name_right}"
+    else
+      cp "${_image}" "${_dirname}/"
+    fi
+  done
+  echo;
+}
+
+for _image_parent_dir in `find "${_image_parentparent_dir}" -mindepth 1 -maxdepth 1 -type d`;
+do
+for _image_dir in `find "${_image_parent_dir}" -mindepth 1 -maxdepth 1 -type d`;
+do
+  _dirname=`basename "${_image_dir}"`
+  if grep "${_image_dir}" "${IGNORE_LIST}" >/dev/null; then
+		continue
+	fi
+  _number_files=`ls -l "${_image_dir}" | wc -l`
+  _count=0
+  _count_targets=0
+  for _image in `find "${_image_dir}" -maxdepth 1 -type f -not -name "*.db"`;
+  do
+    _count=`expr ${_count} + 1`
+    #echo identify -format '%h' "${_image}"
+    #identify -format '%h' "${_image}"
+    _height=`identify -format '%h' "${_image}"`
+		if [ -z ${_height} ]; then
+			echo "${dirname}" "${_image_filename}"
+			#break
+		fi
+    _width=`identify -format '%w' "${_image}"`
+    _image_filename=`basename "${_image}"`
+    printf "                                                                        \r"
+    printf "%-24s %s [%3d(%3d)] - %10s %d %d\r" \
+      "${_dirname:0:24}" "${_dirname/#* }" ${_count_targets} ${_count} "${_image_filename:-10}" ${_height} ${_width}
+    if [ ${_width} -gt ${_height} ]; then
+      _count_targets=`expr ${_count_targets} + 1`
+    fi
+    if [ "${_count_targets}" -gt 10 ]; then
+      echo;
+      image_splitter "${_image_dir}"
+      break
+    fi
+    if [ "${_count}" -gt 30 ]; then
+			echo "${_image_dir}" >> ${IGNORE_LIST}
+      break
+    fi
+  done
+done
+done
+
